30ba190a1ba790b5d90126d225e8c1da9ca5dcd2
Damián morales <60529414+dam788@users.noreply.github.com>
2020-12-18 20:55:50 -0300

    filtro hecho

--- src/components/Porfolio/Porfolio.jsx
+++ src/components/Porfolio/Porfolio.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useContext } from 'react'
+import React, { useContext} from 'react'
 import './porfolio.css'
 import Card from '../Card/Card.jsx'
 import NavCv from '../NavigationCV/NavigationCv.jsx'
@@ -9,39 +9,67 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { ViewContext } from '../../context/ViewContext.jsx'
 import image1 from '../../assets/img/proyects/1.png'
 
-const Porfolio = () => {
 
-  const { proyects, filter, setFilter } = useContext(ViewContext)
+const Porfolio = () => {
+  const { proyects, filter, setFilter} = useContext(ViewContext)
+  
+  const handleCategory = ({target}) => {
+    let category = target.innerText
+    setFilter(category)
+  }
 
   return (
     <>
       <NavCv>
-        <span className="filters" onClick={ ()=>{setFilter('')}           }> Todos </span>
-        <span className="filters" onClick={ ()=>{setFilter('adobe xd')}   }> AdobeXD </span>
-        <span className="filters" onClick={ ()=>{setFilter('maquetado')}  }> Maquetado </span>
-        <span className="filters" onClick={ ()=>{setFilter('javascript')} }> Javascript </span>
-        <span className="filters" onClick={ ()=>{setFilter('react')}   }> React </span>
-        <span className="filters" onClick={ ()=>{setFilter('node')}    }> NodeJS </span>
+        <span set-name="todo"  className="filters" onClick={ handleCategory }>Todo</span>
+        <span set-name="xd"  className="filters" onClick={ handleCategory }>Adobe XD</span>
+        <span set-name="maq"  className="filters" onClick={ handleCategory }>Maquetado</span>
+        <span set-name="js"  className="filters" onClick={ handleCategory }>Javascript</span>
+        <span set-name="react"  className="filters" onClick={ handleCategory }>React</span>
+        <span set-name="node"  className="filters" onClick={ handleCategory }>Node JS</span>
       </NavCv>
-      { filter
-        ? <h2 className="h2Title"><b className="text-white">Proyectos:</b> {filter.toUpperCase()}</h2>
-        : <h2 className="h2Title"><b className="text-white">Proyectos:</b> TODOS</h2>
+      { 
+        <h2 className="h2Title"><b className="text-white">Proyectos:</b> {filter.toUpperCase()}</h2>
       }
       <div className="contentCards">
-      { proyects
-        .map( proyect => (   
-          <Card
-            key={ proyect.title }
-            titulo={ proyect.title }
-            parrafo={ proyect.description }
-            img={ image1 } 
-            icon1={ faGithub } 
-            icon2={ faEnvelope }
-            href1={ proyect.link1 }
-            href2={ proyect.link2 } 
-            textColor={ 'text-dark' }
-          />
-        ))}
+      { filter === 'Todo'?
+        (
+          proyects.map( proyect => {
+            return(
+              <Card
+                key={ proyect.title }
+                titulo={ proyect.title }
+                parrafo={ proyect.description }
+                img={ image1 } 
+                icon1={ faGithub } 
+                icon2={ faEnvelope }
+                href1={ proyect.link1 }
+                href2={ proyect.link2 } 
+                textColor={ 'text-dark' }
+              />
+            )
+          })               
+        ):(
+          proyects.map( proyect => {
+            let {tecnology:tecno} = proyect
+            const match = tecno.find( elem => elem === filter )
+            if( match ) {
+              return(
+                <Card
+                  key={ proyect.title }
+                  titulo={ proyect.title }
+                  parrafo={ proyect.description }
+                  img={ image1 } 
+                  icon1={ faGithub } 
+                  icon2={ faEnvelope }
+                  href1={ proyect.link1 }
+                  href2={ proyect.link2 } 
+                  textColor={ 'text-dark' }
+                />
+              )
+          }})        
+        )  
+      }
       </div>
     </>
   )
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+export const proyects = [
+  {
+    title: 'FrontEnd Mentor 1', 
+    description:'Es un challenge prototipado y Maquetado en HTM,CSS', 
+    photo: '', 
+    link1: '', 
+    link2: '',
+    tecnology: ['Adobe XD','Maquetado']
+  },{
+    title: 'FrontEnd Mentor 2', 
+    description:'Es un challenge prototipado y Maquetado en HTM,CSS', 
+    photo: '', 
+    link1: '', 
+    link2: '',
+    tecnology: ['Adobe XD','Maquetado']
+  },{
+    title: 'Ecommerce Nerd King', 
+    description:'Un ecommerce desarrollado en HTML, CSS y Javascript,la lógica es solo a prueba.', 
+    photo: '', 
+    link1: '', 
+    link2: '',
+    tecnology: ['Javascript', 'Maquetado']
+  },{
+    title: 'Juego Ahorcado', 
+    description:'Juego desarrollado en HTML, CSS y Javascript', 
+    photo: '', 
+    link1: '', 
+    link2: '',
+    tecnology: ['Javascript', 'Maquetado']
+  }
+]
